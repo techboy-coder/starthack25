c46f7a03dd28b571d2d6c570c3206818e75f5721
Shivram Sambhus <shivi@shivi.io>
2025-03-20 16:16:22 +0100

    stuff seems to be working atm

--- services/webapp/src/routes/app/dashboard/+page.svelte
+++ services/webapp/src/routes/app/dashboard/+page.svelte
@@ -7,32 +7,41 @@
 	import * as Tabs from '$lib/components/ui/tabs/index.js';
 	import Dashboard from './charts/dashboard.svelte';
 	import Table from './table/table.svelte';
+	import Flowscore from './flowscore/flowscore.svelte';
 </script>
 
 <div class="container py-20 text-foreground">
 	<h2 class="text-4xl font-bold">Dashboard</h2>
 	<Tabs.Root value="graph" class="w-full py-6">
 		<Tabs.List>
-			<Tabs.Trigger value="table">Data</Tabs.Trigger>
 			<Tabs.Trigger value="graph">Graph</Tabs.Trigger>
+			<Tabs.Trigger value="score">Score</Tabs.Trigger>
+			<Tabs.Trigger value="table">Data</Tabs.Trigger>
 		</Tabs.List>
-		<Tabs.Content value="table">
-			{#await data.data}
-				Loading...
-			{:then val}
-				<Table data={val}></Table>
-			{/await}
-		</Tabs.Content>
 		<Tabs.Content value="graph">
 			<div class="grid w-full grid-cols-1 gap-4 py-6">
 				<Card.Root class="border-2 border-border bg-muted/20 p-6">
-					{#await data.data}
+					{#await data.dpoint}
 						Loading...
 					{:then val}
 						<Dashboard data={val}></Dashboard>
 					{/await}
 				</Card.Root>
 			</div>
+		</Tabs.Content>
+		<!-- <Tabs.Content value="score">
+			<div class="grid w-full grid-cols-1 gap-4 py-6">
+				<Card.Root class="border-2 border-border bg-muted/20 p-6">
+					<Flowscore data={data.fscore}></Flowscore>
+				</Card.Root>
+			</div>
+		</Tabs.Content> -->
+		<Tabs.Content value="table">
+			{#await data.dpoint}
+				Loading...
+			{:then val}
+				<Table data={val}></Table>
+			{/await}
 		</Tabs.Content>
 	</Tabs.Root>
 </div>
--- services/webapp/src/routes/app/dashboard/+page.ts
+++ services/webapp/src/routes/app/dashboard/+page.ts
@@ -3,13 +3,17 @@ import { currentConfig, IDs } from '$lib/app/state.svelte';
 import type { PageLoad } from './$types';
 
 export const load = (async () => {
-    let ids = await getIds();
-    IDs.splice(0, IDs.length, ...[...new Set(ids)]);
-    if (!currentConfig.chosenId || !IDs.includes(currentConfig.chosenId)){
-        currentConfig.chosenId = IDs[0];
-    }
-    return {
-        datapoint: getData(currentConfig.chosenId, currentConfig.limit, currentConfig.skip),
-        flowscore: getFlowScore(currentConfig.chosenId),
-    };
+	let ids = await getIds();
+	IDs.splice(0, IDs.length, ...[...new Set(ids)]);
+	if (!currentConfig.chosenId || !IDs.includes(currentConfig.chosenId)) {
+		currentConfig.chosenId = IDs[0];
+	}
+
+	let dpoint = await getData(currentConfig.chosenId, currentConfig.limit, currentConfig.skip)
+	let fscore =  await getFlowScore(currentConfig.chosenId);
+	
+	return {
+		dpoint: getData(currentConfig.chosenId, currentConfig.limit, currentConfig.skip)
+		// fscore
+	};
 }) satisfies PageLoad;
